4c88feea464b9e984e27aabe9dfc017b896c80f6
Maurício Santos <mauwebjc@gmail.com>
2021-09-13 23:45:13 -0300

    Estrutura base do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,80 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const HeaderWrapper = styled.header`
+  background-color: #FFB564;
+  padding: 8px 16px;
+  display: flex;
+  div {
+    width: 100%;
+    max-width: 1256px;
+    margin: auto;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    div {
+      font-size: 32px;
+      font-weight: bolder;
+      p {
+        margin-left:16px;
+      }
+    }
+    ul {
+      display: flex;      
+      li {
+        padding: 0 16px;
+        border-right: solid 1px #000;
+        :last-child {
+          border-right: none;
+        }
+        :first-child {
+          min-width: 121px;
+          align-items: center;
+          display: flex;
+          font-size: 16px;
+        }
+      }
+    }
+  }
+`;
+
+const ContainerWrapper = styled.div`
+  background-image: url("/images/background-home.jpg")
+`;
+
+function Container() {
+  return (
+    <ContainerWrapper>
+      <div>
+        123456789
+      </div>
+    </ContainerWrapper>
+  );
+}
+
+function Header() {
+  return (
+    <HeaderWrapper>
+      <div>
+        <div>
+          <img src="/images/pizza.png" alt="Logo da Pizzaria Corleone" />
+          <p>Cantina Don Corleone</p>
+        </div>
+        <ul>
+          <li>Desde 1970</li>
+          <li><img src="/images/instagram.png" alt="Instagram da Pizzaria Corleone" /></li>
+          <li><img src="/images/facebook.png" alt="Facebook da Pizzaria Corleone" /></li>
+        </ul>
+      </div>
+    </HeaderWrapper>
+  );
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <>
+      <Header />
+      <Container />
+    </>
+  );
 }
---
+++ src/theme/index.js
@@ -0,0 +1,8 @@
+import breakpoints from './breakpoints';
+
+export default {
+  breakpoints,
+  borderRadius: '8px',
+  transition: '200ms ease-in-out',
+  fontFamily: '\'Rubik\', sans-serif',
+};
