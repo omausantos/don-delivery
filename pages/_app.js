084006c6d98863a46c134a0fa4a2ef58b967b5b2
Maurício Santos <mauwebjc@gmail.com>
2021-09-14 02:03:16 -0300

    Criação de página Home

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import Head from 'next/head';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../src/GlobalStyle';
@@ -13,6 +14,12 @@ const theme = {
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Calistoga&family=Didact+Gothic&display=swap" rel="stylesheet" />
+        <title>Cantina Don Corleone - Desde 1970</title>
+      </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
